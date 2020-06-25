ad7fcc21d92dd0363e6ad6a2f21e22525f649f9b
홍현경 <nowhhk@gmail.com>
2020-06-25 17:43:39 +0900

    리액트 컴포넌트 타입스크립트로 작성하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Greetings from "./Greeting";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+  const onClick = (name: string) => {
+    console.log(`${name} says hello`);
+  };
+  return <Greetings name="hk" onClick={onClick} />;
+  // 필요한 props 를 빠뜨리게 된다면  에디터에 오류가 나타난다.
+};
 
 export default App;
